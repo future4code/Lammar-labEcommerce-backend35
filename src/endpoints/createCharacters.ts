a23e334a70335522f2eed75bdf6570658e1870bf
unknown <vmotta95@gmail.com>
2022-11-29 21:01:29 -0300

    feat-revisao-backend

---
+++ src/endpoints/createCharacters.ts
@@ -0,0 +1,22 @@
+
+import { Request,Response } from "express";
+import {characters} from "../data"
+
+export default function createCharacter (
+    req:Request,
+    res:Response
+    ) : void{
+
+    const {name,gender,description} = req.body
+
+    console.log(req.headers["content-Type"])
+
+    characters.push({
+        id:Date.now(),
+        name,
+        gender,
+        description
+    })
+
+        res.status(201).send()
+    }
---
+++ src/endpoints/getAllCharacters.ts
@@ -0,0 +1,7 @@
+import { Request,Response } from "express";
+import {characters} from "../data"
+export default function (
+    req:Request,
+    res:Response) {
+        res.send(characters)
+    }
